adc730e150a707b8cc86e65046569a9552501d1a
Johnny Machado <johnnymachado490@gmail.com>
2025-11-25 08:31:49 -0500

    Resolve merge issues

---
+++ src/pages/home.tsx
@@ -0,0 +1,25 @@
+import { NavBar } from "@/components/NavBar";
+import { ThemeToggle } from "../components/ThemeToggle";
+import {StarBackground} from '@/components/StarBackground';
+import { HeroSection } from "@/components/HeroSection";
+import { AboutSection } from "@/components/AboutSection";
+import { ProjectSection } from "@/components/ProjectSection";
+
+export const Home = () => {
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/*Theme Toggle*/}
+        <ThemeToggle />
+        {/* Background Effects */}
+        <StarBackground />
+        {/* NavBar */}
+        <NavBar />
+        {/* Main Content */}
+        <main>
+            <HeroSection />
+            <AboutSection />
+            <ProjectSection />
+        </main>
+
+        {/* Footer */}
+        </div>;
+}
