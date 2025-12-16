d98ef7fbd7c4e37641268b8d6f92e9bb9b2953db
johnny <johnnymachado490@gmail.com>
2025-12-16 18:16:36 -0500

    Final touches on the base of the portfolio website

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ import {StarBackground} from '@/components/StarBackground';
 import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
 import { ProjectSection } from "@/components/ProjectSection";
+import { ContactSection } from "@/components/ContactSection";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -18,6 +19,7 @@ export const Home = () => {
             <HeroSection />
             <AboutSection />
             <ProjectSection />
+            <ContactSection />
         </main>
 
         {/* Footer */}
